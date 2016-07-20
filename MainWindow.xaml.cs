7df858d967debefb00dc64e682857d93ed22e180
Librazy <librazy@foxmail.com>
2016-07-20 10:35:43 +0800

    -

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,10 +20,43 @@ namespace LiCalculatorWPF
         #region Core Function
         public MainWindow()
         {
+            var H = 4;
+            var W = 5;
             InitializeComponent();
             MWindow.SourceInitialized += WinSourceInitialized;
+            GameGrid.RowDefinitions.Clear(); 
+            GameGrid.ColumnDefinitions.Clear();
+            GameGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(20,GridUnitType.Star) });
+            for (int i = 0; i < H - 1; ++i) {
+                GameGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(10, GridUnitType.Star), MaxHeight = 15 });
+                GameGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(50, GridUnitType.Star) });
+            }
+            GameGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(10, GridUnitType.Star), MaxHeight = 15 });
+            GameGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(20,GridUnitType.Star) });
+
+            GameGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(20, GridUnitType.Star) });
+            for (int i = 0; i < W - 1; ++i)
+            {
+                GameGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(10, GridUnitType.Star), MaxWidth = 15 });
+                GameGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(50, GridUnitType.Star) });
+            }
+            GameGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(10, GridUnitType.Star), MaxWidth = 15 });
+            GameGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(20, GridUnitType.Star) });
+
+            for (int i = 0; i < H; ++i)
+            {
+                for (int j = 0; j < W; ++j)
+                {
+                    var b = new Button() { Style = (Style)FindResource("DotButton"), };
+                    Grid.SetRow(b, i*2+1);
+                    Grid.SetColumn(b, j*2+1);
+                    GameGrid.Children.Add(b);
+                }
+            }
             var rec = new Button() { Style = (Style)FindResource("LineButton"), };
             rec.Command = LineButtonClick;
+            rec.MouseEnter += new MouseEventHandler(LineButtonOnME);
+            rec.MouseLeave += new MouseEventHandler(LineButtonOnML);
             Grid.SetRow(rec,3);
             Grid.SetColumn(rec, 2);
             GameGrid.Children.Add(rec);
@@ -35,7 +68,7 @@ namespace LiCalculatorWPF
         /// <param name="from">起始颜色</param>
         /// <param name="to">终止颜色</param>
         /// <param name="t">时间 秒</param>
-        private static void SetBGTransform(Control b, Color from, Color to, double t)
+        private static void SetBGTransform(Control b, Color from, Color to, double t, bool fg = false)
         {
             var brush = new SolidColorBrush();
 
@@ -47,7 +80,14 @@ namespace LiCalculatorWPF
             };
 
             brush.BeginAnimation(SolidColorBrush.ColorProperty, colorAnimation, HandoffBehavior.Compose);
-            b.Background = brush;
+            if (fg)
+            {
+                b.Foreground = brush;
+            }
+            else
+            {
+                b.Background = brush;
+            }
         }
         #endregion
 
@@ -156,12 +196,26 @@ namespace LiCalculatorWPF
 
         #endregion
 
+        #region MainWindow Events 
+
+        private void LineButtonOnME(object sender, MouseEventArgs e)
+        {
+            SetBGTransform((Button)sender, Color.FromRgb(78, 201, 178), Color.FromRgb(16, 81, 51), 0.1, true);
+        }
+
+        private void LineButtonOnML(object sender, MouseEventArgs e)
+        {
+            SetBGTransform((Button)sender, Color.FromRgb(16, 81, 51), Color.FromRgb(78, 201, 178), 0.3, true);
+        }
+
+        #endregion
+
         #region Commands
 
         private RelayCommand<Button> _lineButtonClick;
 
-        public RelayCommand<Button> LineButtonClick => _lineButtonClick ??
-                                                        (_lineButtonClick = new RelayCommand<Button>((s)=>AddLine(s,Colors.Aqua)));
+        public RelayCommand<Button> LineButtonClick 
+            => _lineButtonClick ?? (_lineButtonClick = new RelayCommand<Button>((s)=>AddLine(s,Colors.Aqua)));
 
         public void AddLine(Button s,Color t)
         {
